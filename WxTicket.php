f1cf029b9ac8724d57016c8a60e0fbc4234a585e
china_wangyu <china_wangyu@aliyun.com>
2018-01-25 21:40:11 +0800

    更新头部注释 2018-1-25 21：40 wene

--- WxTicket.php
+++ WxTicket.php
@@ -1,6 +1,15 @@
 <?php
 /**
+ *  ** 求职区 **
+ *  期望城市： 成都
+ *  期望薪资： 8k - 12k
  *
+ *  个人信息
+ *
+ *  工作经验: 3年
+ *  开发语言: PHP / Python
+ *
+ *  联系方式：china_wangyu@aliyun.com
  * @date    2018-01-23 17:13:04
  * @version 1.0
  * @authors wene (china_wangyu@aliyun.com)
