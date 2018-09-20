3f005da549ed86ef410b1c08be278ac4e8c64026
china_wangyu <china_wangyu@aliyun.com>
2018-09-20 16:18:00 +0800

    优化了一些BUG

--- WxToken.php
+++ WxToken.php
@@ -23,16 +23,16 @@ class WxToken extends WxBase
         $param = \wechat\lib\File::param('access_token');
         if ($param === null) {
             /****************      进行微信AppID 和 AppSecret的验证   ******************/
-            (empty($appid) or empty($appSecret)) && \wechat\lib\Abnormal::error('请设置管理端微信公众号开发者APPID 和 APPSECRET~ !');
-            (!is_string($appid) or !is_string($appSecret)) && \wechat\lib\Abnormal::error('微信公众号开发者APPID 和 APPSECRET格式错误~ !');
+            (empty($appid) or empty($appSecret)) && self::error('请设置管理端微信公众号开发者APPID 和 APPSECRET~ !');
+            (!is_string($appid) or !is_string($appSecret)) && self::error('微信公众号开发者APPID 和 APPSECRET格式错误~ !');
             /****************      获取参数验证规则      ******************/
             if (strlen(trim($appid)) == 18 or strlen(trim($appSecret)) == 18) {
                 $access_token_url              = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $appid . '&secret=' . $appSecret;
-                $result                        = self::curl_request($access_token_url, true);
+                $result                        = self::get($access_token_url);
                 \wechat\lib\File::param('access_token',$result);
                 return $result['access_token'];
             } else {
-                \wechat\lib\Abnormal::error('请设置正确格式的微信公众号开发者APPID 和 APPSECRET~ !');
+                self::error('请设置正确格式的微信公众号开发者APPID 和 APPSECRET~ !');
             }
         } else {
             return $param['access_token'];
--- lib/File.php
+++ lib/File.php
@@ -70,15 +70,15 @@ class File
      */
     private static function mkdir(string $type = 'param')
     {
-        $file_dir = dirname(__FILE__) . static::$ext . 'log' . static::$ext . date('Y-m-d-H') . static::$ext;
+        $file_dir = dirname(__FILE__) . static::$ext . 'log' ;
+        !is_dir($file_dir) &&  mkdir($file_dir, 0775);
+        $file_dir .=  static::$ext . date('Y-m-d-H') . static::$ext;
         if ($type == 'param') {
             $file_dir = dirname(__FILE__) . static::$ext . 'log' . static::$ext . 'param' . static::$ext;
         }
 
         $file_name = $type . '.log';
-        if (!is_dir($file_dir)) {
-            mkdir($file_dir, 0775);
-        }
+        !is_dir($file_dir) && mkdir($file_dir, 0775);
 
         if (!is_file($file_dir . $file_name)) {
             file_put_contents($file_dir . $file_name, '');
